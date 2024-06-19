3dbc2de29a047e09b9a12878e2b36e138d5530f7
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-06-19 16:23:52 -0300

    adicionando conteúdo na página sobre mim

--- src/routes/AboutMe/AboutMe.jsx
+++ src/routes/AboutMe/AboutMe.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './index.scss'
+import AboutImg from '../../assets/eu.png'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='aboutme container'>
+      <img src={AboutImg} alt="" />
+      <div className='aboutme-container'>
+        <h3>Sobre mim</h3>
+        <p>Olá, sou Maria Júlia Miosso, e sou apaixonada por arte e tecnologia. Criei este site como uma plataforma para compartilhar minhas criações artísticas e, ao mesmo tempo, aprimorar minhas habilidades de desenvolvimento web.</p>
+        <p>mjuliamiosso@gmail.com</p>
+      </div>
+    </div>
   )
 }
 
